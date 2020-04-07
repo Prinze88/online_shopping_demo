acb291793acfc749599a2835187eaa61314a5e38
Prinze88 <batuck420@gamil.com>
2020-04-07 12:08:55 +0530

    Static resources updates

---
+++ onlineShoppingFrontend/src/main/java/com/j2ee/onlineShoppingFrontend/controller/PageController.java
@@ -0,0 +1,30 @@
+package com.j2ee.onlineShoppingFrontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","Hello Moto");
+		return mv;
+	}
+
+
+	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting",required = false) String greeting) {
+		
+		if(greeting==null) {
+			greeting = "Hey Hold onn!!";
+		}
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting" ,greeting);
+		return mv;
+	}
+}
